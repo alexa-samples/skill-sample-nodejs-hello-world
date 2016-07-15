242ea6aff5d0e3a2436cebe01f98b8636073a2f1
Diego Benitez <diegoben@amazon.com>
2016-07-15 10:14:21 -0700

    Initial commit of hello world sample skill.

---
+++ src/index.js
@@ -0,0 +1,20 @@
+'use strict';
+var Alexa = require("alexa-sdk");
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('SayHello');
+    },
+    'HelloWorldIntent': function () {
+        this.emit('SayHello')
+    },
+    'SayHello': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
