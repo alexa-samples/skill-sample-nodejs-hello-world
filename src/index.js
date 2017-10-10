8c2be231edb078732a794211bba31b9a39ffd8a1
akersh-s <akersh.s@gmail.com>
2017-10-10 10:39:07 -0700

    Added Interaction Model and required intent handlers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 'use strict';
-var Alexa = require("alexa-sdk");
+const Alexa = require("alexa-sdk");
 
 exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
+    const alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
-var handlers = {
+const handlers = {
     'LaunchRequest': function () {
         this.emit('SayHello');
     },
@@ -16,6 +16,21 @@ var handlers = {
     },
     'SayHello': function () {
         this.response.speak('Hello World!');
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = 'This is the Hello World Sample Skill. ';
+        const reprompt = 'Say hello, to hear me speak.';
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak('Goodbye!');
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak('See you later!');
         this.emit(':responseReady');
     }
 };
